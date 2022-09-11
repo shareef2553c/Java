0290e9f1bdb777c4b2e20d35c2c3d303892d7cc1
shareef2553c <shareef2553c@gmail.com>
2022-09-11 18:21:19 +0530

    Initial Commit

--- src/main/java/com/training/test/TestMain.java
+++ src/main/java/com/training/test/TestMain.java
@@ -2,6 +2,6 @@ package com.training.test;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("main() executed successfully");
+        System.out.println("main method called successfully");
     }
 }
