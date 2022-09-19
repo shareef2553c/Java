198c38be479e8bbe2ff93831491a7e63ef408da7
shareef2553c <shareef2553c@gmail.com>
2022-09-19 17:23:39 +0530

    Added 2nd line in TestMain file

--- src/main/java/com/training/test/TestMain.java
+++ src/main/java/com/training/test/TestMain.java
@@ -3,5 +3,6 @@ package com.training.test;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("main method called successfully");
+        System.out.println("2nd line added");
     }
 }
